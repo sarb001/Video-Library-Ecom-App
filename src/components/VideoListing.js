1586b73e3f66ecfd33ad9820fc5c668b62d3501d
SARB <mrsinghbusiness05@gmail.com>
2023-07-27 18:24:27 +0530

    frontend done - home page

--- src/components/VideoListing.js
+++ src/components/VideoListing.js
@@ -9,11 +9,18 @@ const VideoListing = () => {
                 <NavMenu />
               </div>
              <div className="sidebar-container" style = {{backgroundColor:'#dee2e6'}}>
-                <span> All1 </span>
-                <span> Stock2  </span>
-                <span> Real3  </span>
-                <span> Real4  </span>
-                <span> Real5  </span>
+
+                    <div className="top-categories" style = {{paddingTop:'2%',cursor:'pointer'}}>
+                        <span style = {{padding:'1% 4%',backgroundColor:'royalblue',borderRadius:'50px'}}> All </span>
+                        <span style = {{padding:'1% 4%',backgroundColor:'royalblue',borderRadius:'50px'}}> Stock2  </span>
+                        <span style = {{padding:'1% 4%',backgroundColor:'royalblue',borderRadius:'50px'}}> Real3  </span>
+                        <span style = {{padding:'1% 4%',backgroundColor:'royalblue',borderRadius:'50px'}}> Real4  </span>
+                        <span style = {{padding:'1% 4%',backgroundColor:'royalblue',borderRadius:'50px'}}> Real5  </span>
+                    </div>
+
+                    <div className="videos-section">
+                        
+                    </div>
              </div>
         </div>
     </>
